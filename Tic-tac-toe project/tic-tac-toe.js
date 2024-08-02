6776d2508e48441ab915e9daac90241c4cee9ee0
Charles <172846903+Ghost-Writer-2@users.noreply.github.com>
2024-08-02 22:58:01 +0100

    Tried to complete the project properly still gonna change it

--- Tic-tac-toe project/tic-tac-toe.js
+++ Tic-tac-toe project/tic-tac-toe.js
@@ -1,27 +1,130 @@
-// Going to research
+const gridCells = document.querySelectorAll('.cell');
+let text = 'x';
+let winner;
 
-const cell = document.querySelectorAll('.cell')
-let player = 'O';
+gridCells.forEach((value, i, num) => {
+  value.addEventListener('click', () => {
+    checkWin(num);
+    if (value.textContent === '') {
+      value.textContent = text;
 
-cell.forEach((value, index) => {
-  value.addEventListener('click', (event) => {
-      checkPlayer(value)
+      if (text === 'x') {
+        text = 'o';
+      } else if (text === 'o') {
+        text = 'x';
+      }
+    } else if (value.textContent !== '') {
+      document.body.innerHTML = `
+    <div class="end-game-box">
+     <p class="end-game">Game Over It's a tie.
+        <br>  Refresh to rematch 
+      </p>
+    </div>
+   `;
+    }
+
+    console.log(winner);
   })
 })
 
-function checkPlayer(value) {
-  if (player === 'X') {
-    player = 'O'
-  } else if (player === 'O') {
-    player = 'X';
-    console.log(player)
-  };
+function checkWin(num) {
+  // Check row
 
- 
-  if (value.innerHTML === player) {
-    null;
-  } else {
-    value.innerHTML = player;
-  };
+  if ((num[0].textContent === 'x' && num[1].textContent === 'x') && num[2].textContent === 'x') {
+    winner = 'Player X';
+    endGame();
 
+  } else if ((num[3].textContent === 'x' && num[4].textContent === 'x') && num[5].textContent === 'x') {
+    winner = 'Player X';
+    endGame();
+
+  } else if ((num[6].textContent === 'x' && num[7].textContent === 'x') && num[8].textContent === 'x') {
+    winner = 'Player X';
+    endGame();
+
+    // check column
+
+  } else if ((num[0].textContent === 'x' && num[3].textContent === 'x') && num[6].textContent === 'x') {
+    winner = 'Player X'; 
+    endGame();
+
+  } else if ((num[1].textContent === 'x' && num[4].textContent === 'x') && num[7].textContent === 'x') {
+    winner = 'Player X';
+    endGame();
+
+  } else if ((num[2].textContent === 'x' && num[5].textContent === 'x') && num[8].textContent === 'x') {
+    winner = 'Player X';
+    endGame();
+
+    // check diagonals
+
+  } else if ((num[0].textContent === 'x' && num[4].textContent === 'x') && num[8].textContent === 'x') {
+    winner = 'Player X';
+    endGame();
+
+  } else if ((num[2].textContent === 'x' && num[4].textContent === 'x') && num[6].textContent === 'x') {
+    winner = 'Player X';
+    endGame();
+  } 
+
+  // Check Player O
+   // Check row
+
+   if ((num[0].textContent === 'o' && num[1].textContent === 'o') && num[2].textContent === 'o') {
+    winner = 'Player O';
+    endGame();
+
+  } else if ((num[3].textContent === 'o' && num[4].textContent === 'o') && num[5].textContent === 'o') {
+    winner = 'Player O';
+    endGame();
+
+  } else if ((num[6].textContent === 'o' && num[7].textContent === 'o') && num[8].textContent === 'o') {
+    winner = 'Player O';
+    endGame();
+
+    // check column
+
+  } else if ((num[0].textContent === 'o' && num[3].textContent === 'o') && num[6].textContent === 'o') {
+    winner = 'Player O'; 
+    endGame();
+
+  } else if ((num[1].textContent === 'o' && num[4].textContent === 'o') && num[7].textContent === 'o') {
+    winner = 'Player O';
+    endGame();
+
+  } else if ((num[2].textContent === 'o' && num[5].textContent === 'o') && num[8].textContent === 'o') {
+    winner = 'Player O';
+    endGame();
+
+    // check diagonals
+
+  } else if ((num[0].textContent === 'o' && num[4].textContent === 'o') && num[8].textContent === 'o') {
+    winner = 'Player O';
+    endGame();
+
+  } else if ((num[2].textContent === 'o' && num[4].textContent === 'o') && num[6].textContent === 'o') {
+    winner = 'Player O';
+    endGame();
+
+  // } else {
+  //   document.body.innerHTML = `
+  //   <div class="end-game-box">
+  //    <p class="end-game">Game Over It's a tie.
+  //       <br>  Refresh to rematch 
+  //     </p>
+  //   </div>
+  //  `
+  }
+
+  return winner;
 }
+
+function endGame() {
+  document.body.innerHTML = `
+    <div class="end-game-box">
+     <p class="end-game">Game Over ${winner} wins.
+        <br>  Refresh to rematch 
+      </p>
+    </div>
+   `; 
+};
